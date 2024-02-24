3b6ae71491fbed6d81fcd606de3ceb150b7f2417
shruti <sryadnik10@gmail.com>
2024-02-24 17:01:55 +0530

    Box order change

--- src/Education.js
+++ src/Education.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function Education() {
     return (
@@ -11,10 +11,9 @@ function Education() {
 
                     <div className="box">
                         <div className="content">
-                            <span> 2007 - 2017 </span>
-                            <h3>secondary school education</h3>
-                            <p>I have done my schooling from Kamala Nehru Kanya Vidyalaya School, Nandurbar, Maharashtra, India
-                            </p>
+                            <span> 2019 - 2023 </span>
+                            <h3>b-tech in instrumentation engineering</h3>
+                            <p> I have successfully completed my B.Tech degree in Instrumentation Engineering from Vishwakarma Institute of Technology, Pune.</p>
                         </div>
                     </div>
 
@@ -22,26 +21,23 @@ function Education() {
                         <div className="content">
                             <span> 2017 - 2019 </span>
                             <h3>higher secondary education</h3>
-                            <p>I have done my junior college from Sri Chaitanya Junior College IIT Academy, Pune, Maharashtra,
-                                India</p>
+                            <p>I have done my junior college from Sri Chaitanya Junior College IIT Academy, Pune, Maharashtra, India</p>
                         </div>
                     </div>
 
                     <div className="box">
                         <div className="content">
-                            <span> 2019 - 2023 </span>
-                            <h3>b-tech in instrumentation engineering</h3>
-                            <p> I have successfully completed my B.Tech degree in Instrumentation Engineering from Vishwakarma
-                                Institute of Technology, Pune.</p>
+                            <span> 2007 - 2017 </span>
+                            <h3>secondary school education</h3>
+                            <p>I have done my schooling from Kamala Nehru Kanya Vidyalaya School, Nandurbar, Maharashtra, India</p>
                         </div>
                     </div>
-
 
                 </div>
 
             </section>
-
         </div>
     )
 }
+
 export default Education;
